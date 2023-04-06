37766dcb94336405582693d40d55dd4a470631b7
ellien <ellien@iap.fr>
2023-04-06 13:36:45 +0200

    060420231336

--- bxa/acis_bxa_opt_selec_vneivneivneipow.py
+++ bxa/acis_bxa_opt_selec_vneivneivneipow.py
@@ -30,7 +30,7 @@ spectrum = sys.argv[1]
 
 
 #path_bxa     = '/home/ellien/CasA/test/acis_bxa_test_powvnei/'
-path_bxa = '/n03data/ellien/CasA/analysis/out3/acis_bxa_opt_selected_vneivneivneipow_%03d/' %int(num_reg)
+path_bxa = '/n03data/ellien/CasA/analysis/out4/acis_bxa_opt_selected_vneivneivneipow_%03d/' %int(num_reg)
 if os.path.isdir( path_bxa ) == False:
     os.makedirs( path_bxa, exist_ok = True )
 shutil.copyfile( os.path.abspath(__file__), os.path.join( path_bxa, 'input.script.py' ) )
