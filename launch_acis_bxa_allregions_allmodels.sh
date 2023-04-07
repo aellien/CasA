158e3d0eda5ea88a17fc12de36a3b0c509415ba5
ellien <ellien@iap.fr>
2023-04-07 16:25:57 +0200

    070420231625

--- launch_acis_bxa_allregions_allmodels.sh
+++ launch_acis_bxa_allregions_allmodels.sh
@@ -3,7 +3,7 @@
 for model in vneivneivneipow
 
 do
-    for spectrum in /n03data/ellien/CasA/data/opt_selected/opt_spec_selected_1.pi
+    for spectrum in /n03data/ellien/CasA/data/opt_selected/opt_spec_selected_*.pi
     do
         echo "Launch ${model} on region ${spectrum}"
         n=$(basename "$spectrum")
