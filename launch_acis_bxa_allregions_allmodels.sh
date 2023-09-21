ccc0d1b49bd0ce33f558485218412296bba4a1c2
ellien <ellien@iap.fr>
2023-09-21 14:58:04 +0200

    opt test 2

--- launch_acis_bxa_allregions_allmodels.sh
+++ launch_acis_bxa_allregions_allmodels.sh
@@ -3,8 +3,8 @@
 for model in gsmoothvneivneivneipow
 
 do
-    for spectrum in /n03data/ellien/CasA/data/Test_spec4x4_within25x25/opt*4_1.pi
-    do
+    for spectrum in /n03data/ellien/CasA/data/Test_spec4x4_within25x25/opt*
+        do
         echo "Launch ${model} on region ${spectrum}"
         n=$(basename "$spectrum")
         qsub qsub_acis_bxa.sh -v spec=${n},mod=${model}
